39b6fafe4db5243dc7349a1498e614866ec1f5e2
wondonghwi <wondonghwi@wondonghwiui-MacBookPro.local>
2019-12-25 01:11:54 +0900

    Updates

---
+++ precache-manifest.c48fc18c1f7c2c25d6ba7be82d87cc01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da0dbd23fe18344a0555d37240b31c29",
+    "url": "/ToDoList-with-RestApi-ver/index.html"
+  },
+  {
+    "revision": "9b6ba3d2ed019841d958",
+    "url": "/ToDoList-with-RestApi-ver/static/css/main.224edff9.chunk.css"
+  },
+  {
+    "revision": "cc9f9ec167b03060a473",
+    "url": "/ToDoList-with-RestApi-ver/static/js/2.8712a06a.chunk.js"
+  },
+  {
+    "revision": "9b6ba3d2ed019841d958",
+    "url": "/ToDoList-with-RestApi-ver/static/js/main.1c3353d1.chunk.js"
+  },
+  {
+    "revision": "10572cba0ffc9f2e6002",
+    "url": "/ToDoList-with-RestApi-ver/static/js/runtime-main.2369b4db.js"
+  }
+]);
